19819573ee83a9e95af69b5b1106d6d7b7ebdd00
PaulCrossman <PaulCrossman@users.noreply.github.com>
2021-08-22 16:49:18 +0100

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+distance = 0
+
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HEART)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    Kitronik_Move_Motor.stop()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    global distance
+    distance = Kitronik_Move_Motor.measure()
+    if distance > 10:
+        Kitronik_Move_Motor.sound_siren(Kitronik_Move_Motor.OnOffSelection.OFF)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 50)
+    elif distance < 10:
+        Kitronik_Move_Motor.stop()
+        Kitronik_Move_Motor.sound_siren(Kitronik_Move_Motor.OnOffSelection.ON)
+        basic.show_string("Excuse me!")
+        basic.pause(500)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.REVERSE, 50)
+        basic.show_string("Bye bye!")
+        basic.pause(500)
+        Kitronik_Move_Motor.sound_siren(Kitronik_Move_Motor.OnOffSelection.OFF)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, 50)
+        basic.pause(500)
+        Kitronik_Move_Motor.stop()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,27 +1,26 @@
 let distance = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Hello!")
-    Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.On)
-    while (input.buttonIsPressed(Button.B) == false) {
-        distance = Kitronik_Move_Motor.measure()
-        if (distance > 10) {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
-        } else if (distance < 10) {
-            Kitronik_Move_Motor.stop()
-            basic.showString("Excuse me!")
-            basic.pause(200)
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
-            basic.showString("Bye bye!")
-            basic.pause(100)
-            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
-            basic.pause(100)
-            Kitronik_Move_Motor.stop()
-        }
-    }
+    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
     Kitronik_Move_Motor.stop()
 })
 basic.forever(function () {
-	
+    distance = Kitronik_Move_Motor.measure()
+    if (distance > 10) {
+        Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.Off)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
+    } else if (distance < 10) {
+        Kitronik_Move_Motor.stop()
+        Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.On)
+        basic.showString("Excuse me!")
+        basic.pause(500)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
+        Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.Off)
+        basic.showString("Bye bye!")
+        basic.pause(500)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
+        basic.pause(500)
+        Kitronik_Move_Motor.stop()
+    }
 })
