543712dc4328b54f3b5367e53da88c02c99f0c91
MrCrossman <12952171+MrCrossman@users.noreply.github.com>
2021-08-23 12:11:18 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,15 +8,12 @@ input.onButtonPressed(Button.B, function () {
 basic.forever(function () {
     distance = Kitronik_Move_Motor.measure()
     if (distance > 10) {
-        Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.Off)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
     } else if (distance < 10) {
         Kitronik_Move_Motor.stop()
-        Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.On)
         basic.showString("Pardon me!")
         basic.pause(500)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
-        Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.Off)
         basic.showString("Bye bye!")
         basic.pause(500)
         Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
