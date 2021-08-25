deba93a2f7b941036981eb81246b684fa1227544
MrCrossman <12952171+MrCrossman@users.noreply.github.com>
2021-08-25 11:34:43 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,9 @@
 let distance = 0
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-})
-input.onButtonPressed(Button.B, function () {
-    Kitronik_Move_Motor.stop()
-})
 basic.forever(function () {
+    basic.showString("Hello!")
     distance = Kitronik_Move_Motor.measure()
     if (distance > 10) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 200)
     } else if (distance < 10) {
         Kitronik_Move_Motor.stop()
         basic.pause(500)
